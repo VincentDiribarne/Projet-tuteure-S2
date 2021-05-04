7042996565c45640955f700d36c1bcee9c98a4e6
Suna24 <bastien.t2002@gmail.com>
2021-05-04 18:54:14 +0200

    Creation de trois pages apres l'accueil + liens entre les boutons + Interface Apercu

--- Code_Ptut/src/Version_Enseignant/Controller.java
+++ Code_Ptut/src/Version_Enseignant/Controller.java
@@ -1,32 +1,160 @@
 package Version_Enseignant;
 
+import java.io.File;
+import java.io.IOException;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.media.*;
 import javafx.stage.*;
 
 public class Controller {
-	
+
+	//Id des variables présentes dans les fichiers FXML
+	@FXML private TextField repertoire;
+	@FXML private MediaView mediaView;
+
+	//Pour l'édition de l'exercice
+	//Consigne
+	@FXML private TextField textFieldConsigne;
+	@FXML private TextArea textAreaConsigne;
+	@FXML private TextField textFieldConsigneFinale;
+
+
+	private void changerScene (Parent root) {
+		Stage stage = (Stage) MainEnseignant.root.getScene().getWindow();
+		MainEnseignant.root = root;
+		stage.setMaximized(true);
+		stage.setScene(new Scene(root));
+		stage.show();
+	}
+
 	//Bouton Quitter qui permet à l'enseignant de quitter l'application
 	@FXML
 	public void quitter(ActionEvent event) {
 		Platform.exit();
 	}
-	
+
 	//Bouton Ouvrir qui permet à l'enseignant d'ouvrir un exercice qu'il à déjà créé auparavant
 	@FXML
 	public void ouvrir(ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        Stage primaryStage = null;
-        fileChooser.setTitle("Open Resource File");
-		fileChooser.showOpenDialog(primaryStage);
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Resource File");
+		fileChooser.showOpenDialog(null);
 		//TODO Chargez l'exercice dans la page
 	}
-	
+
 	//Bouton Nouveau qui permet à l'enseignant de démarrer la création d'un nouvel exercice
 	@FXML
-	public void nouvelExo(ActionEvent event) {
+	public void PageNouvelExo(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("NouvelExo.fxml"));
+		changerScene(root);
+	}
+
+
+	@FXML
+	public void annuler(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("PageAccueil.fxml"));
+		changerScene(root);
+	}
+
+
+	//Méthode pour choisir le répertoire dans lequel l'enseignant enregistrera son fichier
+	@FXML
+	public void choisirRepertoire(ActionEvent event) {
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+		File selectedDirectory;
+		directoryChooser.setTitle("Choisissez un répertoire pour l'enregistrement");
+		selectedDirectory = directoryChooser.showDialog(null);
+		repertoire.setText(selectedDirectory.getAbsolutePath());
+	}
+
+	@FXML
+	public void pageImporterRessource(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ImporterRessource.fxml"));
+		changerScene(root);
+	}
+
+	@FXML
+	public void importerRessource(ActionEvent event) throws IOException {
+		FileChooser fileChooser = new FileChooser();
+		File selectedFile = new File("");
+		selectedFile = fileChooser.showOpenDialog(null);
+		Media media = new Media(selectedFile.toURI().toURL().toExternalForm());
+		MediaPlayer mediaPlayer = new MediaPlayer(media);
+		mediaView.setMediaPlayer(mediaPlayer);
+		mediaPlayer.play();
+	}
+
+	@FXML
+	public void pageApercu(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("PageApercu.fxml"));
+		changerScene(root);
+	}
+
+	//Méthode pour ouvrir une nouvelle fenêtre pour éditer la consigne
+	@FXML
+	public void modifierConsigne(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ModifierConsigne.fxml"));
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+
+		//La fenêtre créée doit être validée et aucune opératio n'est eprmis avec la fenêtre parente
+		stage.initModality(Modality.WINDOW_MODAL);
+		stage.initOwner(MainEnseignant.root.getScene().getWindow());
+		stage.setTitle("Édition de la consigne");
 		
+		stage.show();
+	}
+
+	//Méthode pour ouvrir une nouvelle fenêtre pour éditer la transcription
+	@FXML
+	public void modifierTranscription(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ModifierTranscription.fxml"));
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+
+		//La fenêtre créée doit être validée et aucune opératio n'est eprmis avec la fenêtre parente
+		stage.initModality(Modality.WINDOW_MODAL);
+		stage.initOwner(MainEnseignant.root.getScene().getWindow());
+		stage.setTitle("Édition de la transcription");
+
+		stage.show();
+	}
+
+	//Méthode pour ouvrir une nouvelle fenêtre pour éditer les aides
+	@FXML
+	public void modifierAide(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("ModifierAide.fxml"));
+		Stage stage = new Stage();
+		stage.setScene(new Scene(root));
+
+		//La fenêtre créée doit être validée et aucune opératio n'est eprmis avec la fenêtre parente
+		stage.initModality(Modality.WINDOW_MODAL);
+		stage.initOwner(MainEnseignant.root.getScene().getWindow());
+		stage.setTitle("Édition des aides");
+
+		stage.show();
 	}
 	
+	//Méthode qui remplit le textArea en fonction de ce que contient le TextField
+	@FXML 
+	public void remplissageTexte(KeyEvent event) {
+		//On dit que le texte doit revenir à la ligne
+		textAreaConsigne.setWrapText(true);
+		//On note ce qu'écrit le professeur dans le textArea
+		textAreaConsigne.setText(textFieldConsigne.getText());
+	}
+	
+	@FXML
+	public void validation(ActionEvent event) {
+		textFieldConsigneFinale = new TextField();
+		textFieldConsigneFinale.setText(textFieldConsigne.getText());
+	}
+
 }
--- Code_Ptut/src/Version_Enseignant/MainEnseignant.java
+++ Code_Ptut/src/Version_Enseignant/MainEnseignant.java
@@ -1,100 +1,33 @@
 package Version_Enseignant;
 
-import java.io.File;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.StackPane;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class MainEnseignant extends Application {
-
+	
+	static Parent root;
+	
     @Override
     public void start(Stage primaryStage) throws Exception{
-    	/* Parent root = FXMLLoader.load(getClass().getResource("Enseignant.fxml"));
+         root = FXMLLoader.load(getClass().getResource("PageAccueil.fxml"));
          primaryStage.setTitle("Reconstitution - Version Enseignante");
          
          //On affiche le plein écran
          primaryStage.setMaximized(true);
-         primaryStage.setScene(new Scene(root));
-         primaryStage.show();
-         
+
          //On récupère la largeur et la hauteur de l'écran
-         int width = (int) Screen.getPrimary().getBounds().getWidth();
-         int height = (int) Screen.getPrimary().getBounds().getHeight();
+         //int width = (int) Screen.getPrimary().getBounds().getWidth();
+         //int height = (int) Screen.getPrimary().getBounds().getHeight();
          
          //On restreint la largeur et la hauteur de la page
-         primaryStage.setMinWidth(width - 100);
-         primaryStage.setMinHeight(height - 100);
-       
-         
-		//Tests
-         MediaView mediaView = null;
-         Ressource r = new Ressource("Test", mediaView);*/
- 
-    	final File file = new File("C:\\Users\\basti\\Downloads\\test.mp4"); 
-        final Media media = new Media(file.toURI().toString());
-        final MediaPlayer mediaPlayer = new MediaPlayer(media); 
-        final MediaView mediaView = new MediaView(mediaPlayer); 
-        final StackPane root = new StackPane(); 
-        root.getChildren().setAll(mediaView); 
-        final Scene scene = new Scene(root, 400, 400);
+         //primaryStage.setMinWidth(width - 100);
+         //primaryStage.setMinHeight(height - 100);
         
-       /* Button pause = new Button("Pause");
-        Button play = new Button("Play");
-        Button plus1 = new Button("+1");
-        Button moins1 = new Button("-1");
-        
-        root.getChildren().addAll(pause,play,plus1,moins1);
-        pause.setTranslateX(50);
-        plus1.setTranslateX(100);
-        moins1.setTranslateX(150);
-        
-        //Evenement boutons (avec scene builder)
-        pause.setOnAction(actionEvent -> mediaPlayer.pause());
-        play.setOnAction(actionEvent -> mediaPlayer.play());
-        
-        plus1.setOnAction(actionEvent -> mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(1))));
-        moins1.setOnAction(actionEvent -> mediaPlayer.seek(mediaPlayer.getCurrentTime().add(Duration.seconds(-1))));
-        
-        primaryStage.setFullScreen(true);
-        primaryStage.setTitle("Lecture d'une vidéo"); 
-
-        
-        mediaView.fitWidthProperty().bind(root.widthProperty()); 
-        mediaView.fitHeightProperty().bind(root.heightProperty()); 
-        mediaPlayer.setVolume(mediaPlayer.getVolume() - 0.9);*/
-        
-        Label label = new Label("Votre mode d'exercice: ");
-        label.setTranslateY(-20);
-        // Group
-        ToggleGroup group = new ToggleGroup();
- 
-        // Radio 1: Male
-        RadioButton button1 = new RadioButton("Mode Entraînement");
-        button1.setToggleGroup(group);
-        button1.setSelected(true);
- 
-        // Radio 3: Female.
-        RadioButton button2 = new RadioButton("Mode Évaluation");
-        button2.setToggleGroup(group);
-        button2.setTranslateX(150);
-        
-        root.getChildren().addAll(label, button1, button2);
-        
-        primaryStage.setScene(scene); 
-        primaryStage.show();
+         primaryStage.setScene(new Scene(root));
+         primaryStage.show();
         
     }
     public static void main(String[] args) {
