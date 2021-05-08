8bc5ac217c0280257e8b54e6902b8b6bea3410ff
Vincent Diribarne <vdiribarnepro@gmail.com>
2021-05-08 18:26:53 +0200

    Fin de l'interface de la Page d'accueil

--- Code_Ptut/src/Version_Enseignant/Main.java
+++ Code_Ptut/src/Version_Enseignant/Main.java
@@ -1,23 +1,14 @@
 package Version_Enseignant;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+import java.io.IOException;
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-}
+
--- Code_Ptut/src/Version_Etudiant/Main.java
+++ Code_Ptut/src/Version_Etudiant/Main.java
@@ -6,16 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Dimension tailleMoniteur = Toolkit.getDefaultToolkit().getScreenSize();
+        int longueur = tailleMoniteur.width * 2/3;
+        int hauteur = tailleMoniteur.height * 2/3;
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1920, 1080));
+        primaryStage.setTitle("Reconstitution - Version Etudiante");
+        primaryStage.setScene(new Scene(root, longueur, hauteur));
+        primaryStage.setMinWidth(longueur);
+        primaryStage.setMinHeight(hauteur);
+        primaryStage.setMaximized(true);
+        //primaryStage.setFullScreen(true);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
