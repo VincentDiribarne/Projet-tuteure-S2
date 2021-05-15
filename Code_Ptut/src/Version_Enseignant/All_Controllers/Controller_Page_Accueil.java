54c91329e0c8f3dadc5523fd1a6b0710ec87da4e
Suna24 <bastien.t2002@gmail.com>
2021-05-15 16:57:42 +0200

    Décryptage consigne, transcription, aide, mode, et nbMin

--- Code_Ptut/src/Version_Enseignant/All_Controllers/Controller_Page_Accueil.java
+++ Code_Ptut/src/Version_Enseignant/All_Controllers/Controller_Page_Accueil.java
@@ -1,7 +1,11 @@
 package Version_Enseignant.All_Controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.ByteBuffer;
 import java.util.ResourceBundle;
 
 import Version_Enseignant.MainEnseignant;
@@ -19,7 +23,7 @@ import javafx.stage.Stage;
 public class Controller_Page_Accueil implements Initializable{
 
 	@FXML Label RecupScene;
-	
+
 	//Méthode d'initialisation de la page
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -34,11 +38,95 @@ public class Controller_Page_Accueil implements Initializable{
 
 	//Bouton Ouvrir qui permet à l'enseignant d'ouvrir un exercice qu'il à déjà créé auparavant
 	@FXML
-	public void ouvrir(ActionEvent event) {
+	public void ouvrir(ActionEvent event) throws IOException {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Ouvrez votre exercice");
-		fileChooser.showOpenDialog(null);
+		//Appel de la fonction décrypte pour la fichier sélectionné
+		decrypte(fileChooser.showOpenDialog(null));
 		//TODO Chargez l'exercice dans la page
+	}
+
+	//Fonction qui va load les informations du fichier sélectionné dans les différents TextField...
+	public void decrypte(File file) throws IOException {
+
+		//Variables pour récupérer les informations du fichier
+		String consigne, aide, transcription, caraOccul, nbMin;
+		int nombreOctetALire, sensiCasse, mode, solution, motsDecouverts;
+
+		//On ouvre le fichier en lecture
+		FileInputStream fin = new FileInputStream(file);
+
+		//On récupère la longueur de la consigne + la consigne
+		nombreOctetALire = entier(ByteBuffer.wrap(fin.readNBytes(4)));
+		consigne = chaine(fin.readNBytes(nombreOctetALire));
+		//On met la consigne dans la textField associé
+		Controller_Page_Apercu.contenuConsigne = consigne;
+
+		//On récupère la longueur de la transcription + la transcription
+		nombreOctetALire = entier(ByteBuffer.wrap(fin.readNBytes(4)));
+		transcription = chaine(fin.readNBytes(nombreOctetALire));
+		//On met la transcription dans le textField associé
+		Controller_Page_Apercu.contenuTranscription = transcription;
+
+		//On récupère la longueur de l'aide + l'aide
+		nombreOctetALire = entier(ByteBuffer.wrap(fin.readNBytes(4)));
+		aide = chaine(fin.readNBytes(nombreOctetALire));
+		//On met les aides dans le textField associé
+		Controller_Page_Apercu.contenuAide = aide;
+
+		//On récupère le caractère d'occultation
+		caraOccul = chaine(fin.readNBytes(1));
+		//On met le caractère dans le texField associé
+		Controller_Page_Des_Options.caraOccul = caraOccul;
+
+		//On récupère la reponse de sensiCasse 0 = false, 1 = true
+		sensiCasse = entier(ByteBuffer.wrap(fin.readNBytes(1)));
+		//On met la variable associée en fonction de la réponse
+		if(sensiCasse == 1) {
+			Controller_Page_Des_Options.sensiCasse = true;
+		} else {
+			Controller_Page_Des_Options.sensiCasse = false;
+		}
+
+		//On récupère le mode choisi par l'enseignant 0 = entrainement, 1 = evaluation
+		mode = entier(ByteBuffer.wrap(fin.readNBytes(1)));
+
+		//On met la variable associée en fonction de la réponse
+		//Mode Evaluation
+		if(mode == 1) {
+			Controller_Page_Des_Options.evaluation = true;
+			Controller_Page_Des_Options.entrainement = false;
+
+			nombreOctetALire = entier(ByteBuffer.wrap(fin.readNBytes(4)));
+			nbMin = chaine(fin.readNBytes(nombreOctetALire));
+
+			Controller_Page_Des_Options.nbMin = nbMin;
+			
+			//Mode Entrainement
+		} else {
+			Controller_Page_Des_Options.evaluation = false;
+			Controller_Page_Des_Options.entrainement = true;
+			
+			
+		}
+
+
+
+
+		//Fermeture du fichier
+		fin.close();
+	}
+
+	//Fonction qui transforme un byteBuffer en Int
+	public int entier(ByteBuffer bytes) {
+		return bytes.getInt();
+	}
+
+	//Fonction qui converti des bytes en String
+	public String chaine(byte[] bytes) {
+		//Variable qui contiendra la chaine
+		String chaine = new String(bytes, java.nio.charset.StandardCharsets.UTF_8);
+		return chaine;
 	}
 
 	//Bouton Nouveau qui permet de créer un nouvel exercice
@@ -49,7 +137,7 @@ public class Controller_Page_Accueil implements Initializable{
 		primaryStage.setScene(new Scene(root, MainEnseignant.width, MainEnseignant.height));
 		primaryStage.show();
 	}
-	
+
 	//Bouton Préférences qui emmène sur la page des paramètres
 	@FXML
 	public void preferences(ActionEvent event) throws IOException {
@@ -58,7 +146,7 @@ public class Controller_Page_Accueil implements Initializable{
 		primaryStage.setScene(new Scene(root, MainEnseignant.width, MainEnseignant.height));
 		primaryStage.show();
 	}
-	
+
 	//Bouton DarkMode qui met en darkMode l'application
 	@FXML 
 	public void darkMode() {
